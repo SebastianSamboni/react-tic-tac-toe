3ac9c2c0f78000a2496f394fd6d88d998cac504b
Sebastian Samboni <sebas_18_08@hotmail.com>
2023-10-11 01:10:57 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TURNS = {
+    X: 'x',
+    O: 'o'
 }
 
-export default App;
+const Square = ({ children, isSelected, updateBoard, index }) => {
+    // Constante para definir clase y aplicar estilos por medio de op. ternario donde si se cumple la condición la clase es is-selected, caso contrario, no se aplica clase. Esta constante se asigna como valor del atributo className del div
+    const className = `square ${isSelected ? 'is-selected' : ''}`
+
+    const handleClick = () => {
+        updateBoard(index)
+    }
+    
+    return (
+        <div onClick={handleClick} className={className}>
+            {children}
+        </div>
+    )
+}
+
+function App() {
+    const [board, setBoard] = useState(Array(9).fill(null))
+    
+    const [turn, setTurn] = useState(TURNS.X)
+
+    const updateBoard = (index) => {
+        const newBoard = [...board]
+        newBoard[index] = turn
+        setBoard(newBoard)
+        const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+        setTurn(newTurn)
+    }
+
+    return (
+        <main className='board'>
+            <h1>Tic tac toe</h1>
+            <section className='game'>
+                {
+                    board.map((_, index) => {
+                        return (
+                            <Square key={index} index={index} updateBoard={updateBoard}>
+                                {board[index]}
+                            </Square>
+                        )
+                    })
+                }
+            </section>
+
+            <section className='turn'>
+                <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+                <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+            </section>
+        </main>
+    )
+}
+
+export default App
